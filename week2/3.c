b3a25a5ba864cee2fb5805b2c3b143783472a791
axyn45 <axyn45@outlook.com>
2025-01-16 13:58:53 +1100

    week2: set2-3

---
+++ week2/3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#define SIZE 1000
+
+int *makeArrayOfInts();
+
+int main()
+{
+    int *arr = makeArrayOfInts();
+    printf("%d\n", arr[500]);
+    return 0;
+}
+
+// int *makeArrayOfInts() {
+//    static int arr[1000];
+// //    arr=(int*)malloc(sizeof(int)*1000);
+//    int i;
+//    for (i=0; i<1000; i++) {
+//       arr[i] = i;
+//    }
+//    return arr;
+// }
+
+int *makeArrayOfInts()
+{
+    static int arr[SIZE];
+    int i;
+    for (i = 0; i < SIZE; i++)
+    {
+        arr[i] = i;
+    }
+    return arr;
+}
