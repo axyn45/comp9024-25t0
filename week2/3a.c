49678aa311358fcb007e09b5b249f4287acd9638
axyn45 <axyn45@outlook.com>
2025-01-19 14:31:08 +1100

    week2: change to method of malloc

--- week2/3a.c
+++ week2/3a.c
@@ -11,23 +11,25 @@ int main()
     return 0;
 }
 
-// int *makeArrayOfInts() {
-//    static int arr[1000];
-// //    arr=(int*)malloc(sizeof(int)*1000);
-//    int i;
-//    for (i=0; i<1000; i++) {
-//       arr[i] = i;
-//    }
-//    return arr;
-// }
-
 int *makeArrayOfInts()
 {
-    static int arr[SIZE];
+    static int arr[1000];
+    //    arr=(int*)malloc(sizeof(int)*1000);
     int i;
-    for (i = 0; i < SIZE; i++)
+    for (i = 0; i < 1000; i++)
     {
         arr[i] = i;
     }
     return arr;
 }
+
+// int *makeArrayOfInts()
+// {
+//     static int arr[SIZE];
+//     int i;
+//     for (i = 0; i < SIZE; i++)
+//     {
+//         arr[i] = i;
+//     }
+//     return arr;
+// }
