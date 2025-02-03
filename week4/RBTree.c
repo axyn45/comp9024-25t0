79071ad7422a32a4aa0781082a11f27574d2b97c
axyn45 <axyn45@outlook.com>
2025-02-03 12:05:53 +1100

    week4: RBTree init

---
+++ week4/RBTree.c
@@ -0,0 +1,108 @@
+// Red-Black Tree ADT implementation ... COMP9024 25T0
+
+#include "RBTree.h"
+
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PRINT_COLOR_RED "\x1B[31m"
+#define PRINT_COLOR_RESET "\x1B[0m"
+
+#define data(tree) ((tree)->data)
+#define left(tree) ((tree)->left)
+#define right(tree) ((tree)->right)
+#define color(tree) ((tree)->color)
+#define isRed(tree) ((tree) != NULL && (tree)->color == RED)
+
+typedef enum { RED, BLACK } Colr;
+
+typedef struct Node {
+    Item data;
+    Colr color;
+    Tree left, right;
+} Node;
+
+// create a new empty Tree
+Tree newTree() { return NULL; }
+
+// make a new node containing data
+Tree newNode(Item it) {
+    Tree new = malloc(sizeof(Node));
+    assert(new != NULL);
+    data(new) = it;
+    color(new) = RED;
+    left(new) = right(new) = NULL;
+    return new;
+}
+
+Tree rotateRight(Tree);
+Tree rotateLeft(Tree);
+
+// insert a new item into a tree
+Tree TreeInsert(Tree t, Item it) {
+    printf("Not yet implemented.\n");
+    printf("Returning a fixed tree instead:\n");
+
+    Tree testTree = newNode(2);
+    color(testTree) = BLACK;
+    left(testTree) = newNode(1);
+    right(testTree) = newNode(3);
+    return testTree;
+}
+
+// check whether a key is in a Tree
+bool TreeSearch(Tree t, Item it) {
+    if (t == NULL)
+        return false;
+    else if (it < data(t))
+        return TreeSearch(left(t), it);
+    else if (it > data(t))
+        return TreeSearch(right(t), it);
+    else  // it == data(t)
+        return true;
+}
+
+Tree rotateRight(Tree n1) {
+    if (n1 == NULL || left(n1) == NULL) return n1;
+    Tree n2 = left(n1);
+    left(n1) = right(n2);
+    right(n2) = n1;
+    return n2;
+}
+
+Tree rotateLeft(Tree n2) {
+    if (n2 == NULL || right(n2) == NULL) return n2;
+    Tree n1 = right(n2);
+    right(n2) = left(n1);
+    left(n1) = n2;
+    return n1;
+}
+
+// free memory associated with Tree
+void freeTree(Tree t) {
+    if (t != NULL) {
+        freeTree(left(t));
+        freeTree(right(t));
+        free(t);
+    }
+}
+
+// display Tree sideways
+void showTreeR(Tree t, int depth) {
+    if (t != NULL) {
+        showTreeR(right(t), depth + 1);
+        int i;
+        for (i = 0; i < depth; i++) putchar('\t');  // TAB character
+        if (isRed(t))
+            printf("%s%d%s\n", PRINT_COLOR_RED, data(t), PRINT_COLOR_RESET);
+        else
+            printf("%d\n", data(t));
+        showTreeR(left(t), depth + 1);
+    }
+}
+
+void showTree(Tree t) { showTreeR(t, 0); }
+
+Tree TreeInsert(Tree t, Item it) {}
